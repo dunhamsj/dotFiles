cb1493af84319f16582b6a5d7b3da97dcb635377
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-11-24 00:01:01 -0500

    updates

--- lnsf.sh
+++ lnsf.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 DOTFILES_DIR=/mnt/shared/documents/dotFiles
-XDG_CONFIG_DIR=${HOME}/.config
 
 dir=${HOME}
 ln -sf ${DOTFILES_DIR}/bash_profile ${dir}/.bash_profile
@@ -13,15 +12,15 @@ ln -sf ${DOTFILES_DIR}/gitconfig ${dir}/.gitconfig
 ln -sf ${DOTFILES_DIR}/tmux.conf ${dir}/.tmux.conf
 ln -sf ${DOTFILES_DIR}/xinitrc ${dir}/.xinitrc
 
-dir=${XDG_CONFIG_DIR}/mpd
+dir=${XDG_CONFIG_HOME}/mpd
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/mpd.conf ${dir}
 
-dir=${XDG_CONFIG_DIR}/nvim
+dir=${XDG_CONFIG_HOME}/nvim
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/init.vim ${dir}
 
-dir=${XDG_CONFIG_DIR}/nvim/after/syntax
+dir=${XDG_CONFIG_HOME}/nvim/after/syntax
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/fortran.vim ${dir}
 ln -sf ${DOTFILES_DIR}/python.vim ${dir}
@@ -31,24 +30,24 @@ dir=${HOME}/.ssh
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/sshconfig ${dir}/config
 
-dir=${XDG_CONFIG_DIR}/alacritty
+dir=${XDG_CONFIG_HOME}/alacritty
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/alacritty.toml ${dir}
 
-dir=${XDG_CONFIG_DIR}/i3
+dir=${XDG_CONFIG_HOME}/i3
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/i3.conf ${dir}/config
 
-dir=${XDG_CONFIG_DIR}/i3blocks
+dir=${XDG_CONFIG_HOME}/i3blocks
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/i3blocks.conf ${dir}/config
 ln -sf ${DOTFILES_DIR}/i3blocks_volume ${dir}
 
-dir=${XDG_CONFIG_DIR}/picom
+dir=${XDG_CONFIG_HOME}/picom
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/picom.conf ${dir}
 
-dir=${XDG_CONFIG_DIR}/mpv
+dir=${XDG_CONFIG_HOME}/mpv
 mkdir -p ${dir}
 ln -sf ${DOTFILES_DIR}/mpv.conf ${dir}
 
