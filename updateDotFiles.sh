b32acf9b368a33f27758484ce30ece9e30a6ad28
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2024-12-07 12:36:57 -0500

    updates

--- updateDotFiles.sh
+++ updateDotFiles.sh
@@ -2,7 +2,6 @@
 
 cd /mnt/shared/documents/dotFiles
 git pull
-contab -l > /mnt/shared/documents/dotFiles/crontab.archlinux
 yay -Qqe > /mnt/shared/documents/dotFiles/yayQqe.txt
 git add .
 git commit -m "updates"
