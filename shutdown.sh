120d2a6b7af449b39543d03aac4ad5a536236117
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2026-08-25 11:22:54 -0400

    updates

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
+if [[ ! ${PWD} = / ]]; then
+  echo "Not in /, exiting..."
+  exit
+fi
+
 killall mpd 2> /dev/null || echo "No mpd process running"
-sudo umount /mnt/shared && \
-sudo umount /mnt/lump && \
-sudo shutdown
+sudo umount /mnt/litterbox && \
+sudo umount /.snapshots && \
+sudo umount /home && \
+sudo shutdown now
