f3bd7731f72c455a18817dac355b9338f9d7a2b6
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2026-08-27 00:00:01 -0400

    updates

--- shutdown.sh
+++ shutdown.sh
@@ -5,8 +5,21 @@ if [[ ! ${PWD} = / ]]; then
   exit
 fi
 
-killall mpd 2> /dev/null || echo "No mpd process running"
-sudo umount /mnt/litterbox && \
-sudo umount /.snapshots && \
-sudo umount /home && \
-sudo shutdown now
+killall mpd        2> /dev/null || echo "No mpd processes running"
+killall pulseaudio 2> /dev/null || echo "No pulseaudio processes running"
+
+drive=/mnt/lump
+for line in `lsblk`
+do
+  if [[ ${line} == *"${drive}"* ]]; then
+    sudo umount ${drive}
+    break
+  fi
+done
+
+sudo lxc-stop jellyfin && \
+  sudo lxc-stop nginx && \
+  sudo umount /mnt/litterbox && \
+  sudo umount /.snapshots && \
+  sudo umount /home && \
+  sudo shutdown now
