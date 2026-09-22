fc109b02652fc273c33e54d0badcc7dae5a259cd
Sam Dunham <9784969+dunhamsj@users.noreply.github.com>
2026-09-22 00:00:07 -0400

    updates

--- reboot.sh
+++ reboot.sh
@@ -7,6 +7,7 @@ fi
 
 killall mpd        2> /dev/null || echo "No mpd processes running"
 killall pulseaudio 2> /dev/null || echo "No pulseaudio processes running"
+sudo kill `pgrep vncsession` 2> /dev/null || echo "No VNC session running"
 
 drive=/mnt/lump
 for line in `lsblk`
@@ -17,9 +18,33 @@ do
   fi
 done
 
+# stop lxc containers
 sudo lxc-stop jellyfin && \
   sudo lxc-stop nginx && \
-  sudo umount /mnt/litterbox && \
+
+# unmount /home; systemd doesn't always close
+sudo umount /home
+error=$?
+count=0
+while [[ ${count} -lt 10 && ${error} -ne 0 ]]
+do
+  if [[ ${error} -eq 0 ]]; then
+    break
+  fi
+  echo count=${count}
+  PID=`sudo journalctl -b | grep "Starting D-Bus" \
+    | tail -n 1 | cut -d" " -f5 | cut -d[ -f2 | cut -d] -f1 \
+    | xargs kill`
+  sudo umount /home
+  error=$?
+  count=$((count+1))
+done
+if [[ ${count} -ge 10 ]]; then
+  echo count=$count
+  echo Not rebooting
+  exit
+fi
+
+sudo umount /mnt/litterbox && \
   sudo umount /.snapshots && \
-  sudo umount /home && \
   sudo shutdown -r now
